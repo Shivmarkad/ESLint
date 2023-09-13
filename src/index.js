3e232fd6331b20a4b5d517e05b2aefe442a24e09
Shivganesh <shivmarkad99@gmail.com>
2023-09-13 16:54:41 +0530

    [Shivganesh] Added: linting rule for indentation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 function mynfunc (word) {
-    return word[0];
+    if(word[0] > 1)   
+        return word[0];
+
+    if(word[2]<2){
+        let ele = word[2];
+        return ele;
+    }
+    return word[2];
 }
 
-console.log(mynfunc([1,2,4,4,5]));
+const newCall = mynfunc([1,2,4,4,5]);
